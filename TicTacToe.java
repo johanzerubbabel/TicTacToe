ea0e17eb5e7e8aef7a3e85867f7b0cf481d542f4
johanzerubbabel <johanjm0305@gmail.com>
2026-04-20 09:35:42 +0530

    UC3: Accept user slot input (1-9)

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,59 +1,16 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
-    static boolean isHumanTurn;
-    static char humanSymbol;
-    static char computerSymbol;
-
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
-        tossAndAssignSymbols();
-        displayTossResult();
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
     }
 
-    static void initializeBoard() {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                board[row][col] = '-';
-            }
-        }
-    }
-
-    static void printBoard() {
-        System.out.println("-------------");
-        for (int row = 0; row < 3; row++) {
-            System.out.print("| ");
-            for (int col = 0; col < 3; col++) {
-                System.out.print(board[row][col] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
-        }
-    }
-
-    static void tossAndAssignSymbols() {
-        Random random = new Random();
-        int toss = random.nextInt(2);
-        if (toss == 0) {
-            isHumanTurn = true;
-            humanSymbol = 'X';
-            computerSymbol = 'O';
-        } else {
-            isHumanTurn = false;
-            humanSymbol = 'O';
-            computerSymbol = 'X';
-        }
-    }
-
-    static void displayTossResult() {
-        if (isHumanTurn) {
-            System.out.println("You won the toss! You play first as '" + humanSymbol + "'.");
-        } else {
-            System.out.println("Computer won the toss! Computer plays first as '" + computerSymbol + "'.");
-        }
-        System.out.println("You are '" + humanSymbol + "', Computer is '" + computerSymbol + "'.");
+    static int getUserSlot() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your slot (1-9): ");
+        int slot = scanner.nextInt();
+        return slot;
     }
 }
