1199b584ff56e91fde4e9a51750d69683ce7886b
johanzerubbabel <johanjm0305@gmail.com>
2026-04-29 11:18:58 +0530

    UC9: Check winning condition

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,16 +1,26 @@
 public class TicTacToe {
 
-    static boolean isHumanTurn = true;
-    static boolean gameOver = false;
+    static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-        while (!gameOver) {
-            if (isHumanTurn) {
-                System.out.println("Human's turn");
-            } else {
-                System.out.println("Computer's turn");
+        System.out.println(hasWon('X'));
+    }
+
+    static boolean hasWon(char symbol) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
+                return true;
             }
-            isHumanTurn = !isHumanTurn;
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) {
+                return true;
+            }
         }
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+            return true;
+        }
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
+            return true;
+        }
+        return false;
     }
 }
